36ebf199c70fe7febba13bc4562edecbce57e200
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-08-26 15:21:52 +0700

    thêm thành công!

--- app/src/main/java/com/example/fptentrytest/MainActivity.java
+++ app/src/main/java/com/example/fptentrytest/MainActivity.java
@@ -1,17 +1,47 @@
 package com.example.fptentrytest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.fptentrytest.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper mDataBaseHelper;
+    List<Vehicle> vehicleList;
+    VehicleAdapter vehicleAdapter;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         mDataBaseHelper = new DataBaseHelper(MainActivity.this);
+        vehicleList = new ArrayList<>();
+        vehicleAdapter = new VehicleAdapter(this,vehicleList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL,false);
+        binding.rcvDisplace.setLayoutManager(linearLayoutManager);
+        binding.rcvDisplace.setAdapter(vehicleAdapter);
+        vehicleAdapter.setData(mDataBaseHelper.getEveryOne());
+
+
+
+        binding.btnAddOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDataBaseHelper.addOne(new Vehicle(1,"Honda","Motobike",13));
+                vehicleAdapter.setData(mDataBaseHelper.getEveryOne());
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/fptentrytest/VehicleAdapter.java
@@ -0,0 +1,69 @@
+package com.example.fptentrytest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class VehicleAdapter extends RecyclerView.Adapter<VehicleAdapter.VehicleViewHolder> {
+
+    Context context;
+    List<Vehicle> vehicleList;
+
+    public VehicleAdapter(Context context, List<Vehicle> vehicleList) {
+        this.context = context;
+        this.vehicleList = vehicleList;
+    }
+
+    public void setData(List<Vehicle> data){
+        this.vehicleList = data;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public VehicleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.layout_item_vehicle,parent,false);
+        return new VehicleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VehicleViewHolder holder, int position) {
+        if (vehicleList == null){
+            return;
+        }
+        Vehicle vehicle = vehicleList.get(position);
+        holder.id_vehicle.setText(String.valueOf(vehicle.getId()));
+        holder.name_vehicle.setText(vehicle.getName());
+        holder.type_vehicle.setText(vehicle.getType());
+        holder.price_vehicle.setText(String.valueOf(vehicle.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (vehicleList != null){
+            return vehicleList.size();
+        }
+        return 0;
+    }
+
+    class VehicleViewHolder extends RecyclerView.ViewHolder{
+
+        TextView id_vehicle, name_vehicle, type_vehicle, price_vehicle;
+
+        public VehicleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            id_vehicle = itemView.findViewById(R.id.id_vehicle);
+            name_vehicle = itemView.findViewById(R.id.name_vehicle);
+            type_vehicle = itemView.findViewById(R.id.type_vehicle);
+            price_vehicle = itemView.findViewById(R.id.price_vehicle);
+        }
+    }
+}
